13ed243e70e75782c8410b6d8a32d64708d1841e
Carlos Perez <cmperez0730@gmail.com>
2024-12-21 18:46:25 -0800

    Set Up the React Project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    return (
-        <div>
-            <h1>Welcome to MyMichiCat</h1>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+
+      {/* Test Tailwind CSS */}
+      <div className="mt-6 text-2xl text-blue-600 font-bold">
+        Hello, Tailwind CSS is working!
+      </div>
+    </div>
+  );
 }
 
 export default App;
